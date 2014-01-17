3d2afbf83ec0efa04a29ad604272be8f89db4fbe
noevil <noevil@foxmail.com>
2014-01-18 01:46:15 +0800

    change speed

--- mountdoom/web/js/n.js
+++ mountdoom/web/js/n.js
@@ -2,18 +2,18 @@ $(document).ready(function() {
 	BOX_WIDTH = $('#frame').width() - 200;
 
 	var color = new Array('#ff0', '#f00', '#000', '#00f', '#0ff');
-	if(window.DeviceMotionEvent) {
-		var speed = 25;
+	if (window.DeviceMotionEvent) {
+		var speed = 60;
 		var x = y = z = lastX = lastY = lastZ = 0;
-		window.addEventListener('devicemotion', function(){
-			var acceleration =event.accelerationIncludingGravity;
+		window.addEventListener('devicemotion', function() {
+			var acceleration = event.accelerationIncludingGravity;
 			x = acceleration.x;
 			y = acceleration.y;
-			if(Math.abs(x-lastX) > speed || Math.abs(y-lastY) > speed) {
-				document.body.style.backgroundColor = color[Math.round(Math.random()*10)%6];
+			if (Math.abs(x - lastX) > speed || Math.abs(y - lastY) > speed) {
+				document.body.style.backgroundColor = color[Math.round(Math.random() * 10) % 6];
 
 				// SEND COLOR
-				$('[name="message"]').val(':' + parseInt(Math.random()*(209)+1));
+				$('[name="message"]').val(':' + parseInt(Math.random() * (209) + 1));
 				$('[name="message"]').submit();
 			}
 			lastX = x;
