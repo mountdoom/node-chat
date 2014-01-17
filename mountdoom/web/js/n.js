64df194ff1e21f08d13f4ccc313a9d9252a97a5f
sun <241456911@qq.com>
2014-01-18 01:33:22 +0800

    update yaoyiyao

--- mountdoom/web/js/n.js
+++ mountdoom/web/js/n.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
 	BOX_WIDTH = $('#frame').width() - 200;
+
+	var color = new Array('#ff0', '#f00', '#000', '#00f', '#0ff');
+	if(window.DeviceMotionEvent) {
+		var speed = 25;
+		var x = y = z = lastX = lastY = lastZ = 0;
+		window.addEventListener('devicemotion', function(){
+			var acceleration =event.accelerationIncludingGravity;
+			x = acceleration.x;
+			y = acceleration.y;
+			if(Math.abs(x-lastX) > speed || Math.abs(y-lastY) > speed) {
+				document.body.style.backgroundColor = color[Math.round(Math.random()*10)%6];
+			}
+			lastX = x;
+			lastY = y;
+		}, false);
+	}
 });
