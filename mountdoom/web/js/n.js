a1a4f0c8abbbda332b107d794d573a67e1416707
sun <241456911@qq.com>
2014-01-18 01:47:02 +0800

    update

--- mountdoom/web/js/n.js
+++ mountdoom/web/js/n.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	BOX_WIDTH = $('#frame').width() - 200;
 
-	var color = new Array('#ff0', '#f00', '#000', '#00f', '#0ff');
+	var color = new Array('#ff4d00', '#268f21', '#009cff', '#c1328e', '#006284');
 	if(window.DeviceMotionEvent) {
 		var speed = 25;
 		var x = y = z = lastX = lastY = lastZ = 0;
