eb7610ed5b552bbdf0e0e0f00aadd5c83384b4c2
sun <241456911@qq.com>
2014-01-18 09:03:42 +0800

    update

--- mountdoom/web/js/n.js
+++ mountdoom/web/js/n.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
 	BOX_WIDTH = $('#frame').width() - 200;
 
-	var color = new Array('#ff4d00', '#268f21', '#009cff', '#c1328e', '#006284');
+	var color = new Array('#ff4d00', '#268f21', '#009cff', '#c1328e', '#006284', "#cb4b45", "#e03c8a");
 	if (window.DeviceMotionEvent) {
 		var speed = 30;
 		var x = y = z = lastX = lastY = lastZ = 0;
+		var lock= false;
 		window.addEventListener('devicemotion', function() {
 			var acceleration = event.accelerationIncludingGravity;
 			x = acceleration.x;
 			y = acceleration.y;
-			if (Math.abs(x - lastX) > speed || Math.abs(y - lastY) > speed) {
-				document.body.style.backgroundColor = color[Math.round(Math.random() * 10) % 6];
-
+			if ((Math.abs(x - lastX) > speed || Math.abs(y - lastY) > speed) && !lock) {
+				lock= true;
+				document.body.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
 				// SEND COLOR
 				$('[name="message"]').val(':' + parseInt(Math.random() * (209) + 1));
 				$('[name="message"]').submit();
+				setTimeout(function(){
+					lock= false;
+				}, 500)
 			}
 			lastX = x;
 			lastY = y;
